085eff289547eb4283eba4ad242fb994a9410b4c
James Pettit <etherealmachine@gmail.com>
2016-01-11 08:24:35 -0800

    Swapped out diff_match_patch for difflib.

--- HitRecord.py
+++ HitRecord.py
@@ -1,9 +1,5 @@
-import os, sys
-dist_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, dist_dir)
-
-import diff_match_patch
-import json
+import difflib
+import os
 import sublime, sublime_plugin
 
 class ToggleRecordingCommand(sublime_plugin.TextCommand):
@@ -13,14 +9,14 @@ class ToggleRecordingCommand(sublime_plugin.TextCommand):
 	def run(self, edit, **args):
 		ToggleRecordingCommand.recording = not ToggleRecordingCommand.recording
 		if ToggleRecordingCommand.recording:
-			print('recording ON')
+			sublime.status_message('recording')
 			if not ChangeRecorder.buf:
 				ChangeRecorder.buf = ChangeBuffer(self.view)
-			elif ChangeRecorder.buf.target is not self.view.file_name():
+			elif ChangeRecorder.buf.view != self.view:
 				ChangeRecorder.buf.logfile.close()
 				ChangeRecorder.buf = ChangeBuffer(self.view)
 		if not ToggleRecordingCommand.recording:
-			print('recording OFF')
+			sublime.status_message('')
 			ChangeRecorder.buf.logfile.close()
 
 class PlaybackRecordingCommand(sublime_plugin.TextCommand):
@@ -29,17 +25,24 @@ class PlaybackRecordingCommand(sublime_plugin.TextCommand):
 	count = 0
 
 	def run(self, edit, clear=False):
-		if not PlaybackRecordingCommand.ops:
+		if clear:
+			PlaybackRecordingCommand.ops = None
+		elif not PlaybackRecordingCommand.ops:
 			self.view.erase(edit, sublime.Region(0, self.view.size()))
-			changes = []
+			checkpoints = []
 			with open(os.path.join(os.path.dirname(self.view.file_name()), 'recording'), 'r') as f:
+				lines = []
 				for line in f:
-					changes.append(json.loads(line))
-			PlaybackRecordingCommand.ops = gencheckpoints(changes)
+					if line == 'CHECKPOINT\n':
+						checkpoints.append(''.join(lines))
+						lines = []
+					else:
+						lines.append(line)
+				if lines:
+					checkpoints.append(''.join(lines))
+			PlaybackRecordingCommand.ops = genops(checkpoints)
 			PlaybackRecordingCommand.count = 0
 			self.view.run_command('playback_recording')
-		elif clear:
-			PlaybackRecordingCommand.ops = None
 		else:
 			self.view.run_command('enter_insert_mode')
 			try:
@@ -54,6 +57,11 @@ class PlaybackRecordingCommand(sublime_plugin.TextCommand):
 					self.view.sel().clear()
 					self.view.sel().add(sublime.Region(point))
 					self.view.insert(edit, point, text)
+				elif optype == 'erase':
+					point = opargs['point']
+					self.view.sel().clear()
+					self.view.sel().add(sublime.Region(point))
+					self.view.erase(edit, sublime.Region(point, point+1))
 				elif optype == 'pause':
 					sublime.status_message('?')
 					self.view.run_command('exit_insert_mode')
@@ -64,47 +72,31 @@ class PlaybackRecordingCommand(sublime_plugin.TextCommand):
 				sublime.status_message('\\o/')
 				PlaybackRecordingCommand.ops = None
 
-def gencheckpoints(changes):
-	for change in changes:
-		for op in genops(change):
-			yield op
+def genops(checkpoints):
+	for i, newState in enumerate(checkpoints[1:]):
+		oldState = checkpoints[i]
+		diffs = difflib.ndiff(oldState, newState)
+		point = 0
+		for diff in diffs:
+			if diff[:2] == '  ':
+				point += 1
+			elif diff[:2] == '+ ':
+				yield ('insert', {'point': point, 'text': diff[2:]})
+				point += 1
+			elif diff[:2] == '- ':
+				yield ('erase', {'point': point})
 		yield ('pause', None)
-
-def genops(change):
-	differ = diff_match_patch.diff_match_patch()
-	diff = differ.diff_main(change['oldState'], change['newState'])
-	point = 0
-	for (diff_type, text) in diff:
-		if diff_type == differ.DIFF_EQUAL:
-			point += len(text)
-		elif diff_type == differ.DIFF_INSERT:
-			if text[-1] == '\n':
-				yield ('insert', {'point': point, 'text': '\n'})
-				text = text[:-1]
-			for i, c in enumerate(text):
-				yield ('insert', {'point': point+i, 'text': c})
-			point += len(text)
-		elif diff_type == differ.DIFF_DELETE:
-			continue
 
 class ChangeBuffer(object):
 
 	def __init__(self, view):
 		self.view = view
-		self.target = view.file_name()
-		self.state = view.substr(sublime.Region(0, view.size()))
-		self.logfile = open(os.path.join(os.path.dirname(self.target), 'recording'), 'w')
+		self.logfile = open(os.path.join(os.path.dirname(self.view.file_name()), 'recording'), 'w')
 
-	def checkpoint(self, newState):
-		print('checkpoint')
-		change = {
-			'oldState': self.state,
-			'newState': newState
-		}
-		self.logfile.write(json.dumps(change))
-		self.logfile.write('\n')
+	def checkpoint(self):
+		self.logfile.write('CHECKPOINT\n')
+		self.logfile.write(self.view.substr(sublime.Region(0, self.view.size())))
 		self.logfile.flush()
-		self.state = newState
 
 class ChangeRecorder(sublime_plugin.EventListener):
 
@@ -113,7 +105,5 @@ class ChangeRecorder(sublime_plugin.EventListener):
 	def on_post_save(self, view):
 		if not ToggleRecordingCommand.recording:
 			return
-		if not ChangeRecorder.buf or ChangeRecorder.buf.target != view.file_name():
-			return
-		newState = view.substr(sublime.Region(0, view.size()))
-		ChangeRecorder.buf.checkpoint(newState)
+		if view == ChangeRecorder.buf.view:
+			ChangeRecorder.buf.checkpoint()
