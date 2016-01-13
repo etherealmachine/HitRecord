ec2d4935790f2697b48ca7aeb1aacb0c8bfe0d02
James Pettit <etherealmachine@gmail.com>
2016-01-13 14:19:21 -0800

    Pretty much working.

--- HitRecord.py
+++ HitRecord.py
@@ -7,29 +7,36 @@ import tempfile
 
 class HitRecordCommand(sublime_plugin.TextCommand):
 
-	recording = None
+	recorder = None
 	paused = False
 
-	def run(self, edit, start=False, stop=False, toggle_pause=False):
+	def run(self, edit, start=False, stop=False, resume=False, toggle_pause=False):
 		if start:
 			self.start()
 		elif stop:
 			self.stop()
+		elif resume:
+			self.resume()
 		elif toggle_pause:
 			self.toggle_pause()
 
 	def start(self):
-		if HitRecordCommand.recording:
-			HitRecordCommand.recording.close()
-		HitRecordCommand.recording = Recording(self.view)
+		if HitRecordCommand.recorder:
+			HitRecordCommand.recorder.close()
+		HitRecordCommand.recorder = Recorder(self.view)
 		HitRecordCommand.paused = False
 		sublime.status_message('recording')
 
 	def stop(self):
-		if HitRecordCommand.recording:
-			HitRecordCommand.recording.close()
-			HitRecordCommand.recording = None
+		if HitRecordCommand.recorder:
+			HitRecordCommand.recorder.close()
+			HitRecordCommand.recorder = None
 		sublime.status_message('recording stopped')
+
+	def resume(self):
+		if not HitRecordCommand.recorder:
+			HitRecordCommand.recorder = Recorder(self.view, resume=True)
+		sublime.status_message('recording resumed')
 
 	def toggle_pause(self):
 		if HitRecordCommand.paused:
@@ -40,24 +47,34 @@ class HitRecordCommand(sublime_plugin.TextCommand):
 			sublime.status_message('recording paused')
 
 	def on_post_save(view):
-		if (HitRecordCommand.recording and
+		if (HitRecordCommand.recorder and
 			  not HitRecordCommand.paused and
-			  HitRecordCommand.recording.view == view):
-			HitRecordCommand.recording.checkpoint()
+			  HitRecordCommand.recorder.view == view):
+			HitRecordCommand.recorder.checkpoint()
 
 	def on_close(view):
-		if (HitRecordCommand.recording and
-			  HitRecordCommand.recording.view == view):
-			HitRecordCommand.recording.close()
+		if (HitRecordCommand.recorder and
+			  HitRecordCommand.recorder.view == view):
+			HitRecordCommand.recorder.close()
 
 
-class ViewRecordingCommand(sublime_plugin.TextCommand):
+class ExecInShellCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
-		if not HitRecordCommand.recording:
-			sublime.error_message('no recording found')
-			return
-		self.view.window().open_file(HitRecordCommand.recording.fname)
+	def run(self, edit, command=None, record=False):
+		if command:
+			settings = sublime.load_settings('HitRecord.sublime-settings')
+			ttyecho = settings.get('ttyecho')
+			if ttyecho:
+				tty = settings.get('tty')
+				try:
+					shell_command = '{} {} "{}"'.format(ttyecho, tty, command)
+					output = subprocess.check_output(shell_command, shell=True, stderr=subprocess.STDOUT)
+				except subprocess.CalledProcessError:
+					sublime.error_message('command "{}" failed: {}'.format(command, output))
+				if HitRecordCommand.recorder:
+					HitRecordCommand.recorder.command(command)
+			else:
+				print('ignoring command: no ttyecho set')
 
 
 class Listener(sublime_plugin.EventListener):
@@ -69,40 +86,76 @@ class Listener(sublime_plugin.EventListener):
 		HitRecordCommand.on_close(view)
 
 
-class Recording(object):
+class Recorder(object):
 
-	def __init__(self, view):
+	def __init__(self, view, resume=False):
 		self.view = view
-		self.fname = Recording.name(self.view)
-		self.log = open(self.fname, 'w+')
+		self.fname = Recording(self.view).fname
+		if resume:
+			self.log = open(self.fname, 'a')
+		else:
+			self.log = open(self.fname, 'w')
 
 	def checkpoint(self):
-		self.log.write('CHECKPOINT\n')
 		self.log.write(self.view.substr(sublime.Region(0, self.view.size())))
+		self.log.write('CHECKPOINT\n')
+		self.log.flush()
+
+	def command(self, command):
+		self.log.write('COMMAND ')
+		self.log.write(command)
+		self.log.write('\n')
 		self.log.flush()
 
 	def close(self):
 		self.log.close()
 
-	@staticmethod
-	def name(view):
-		dirname, basename = os.path.split(view.file_name())
-		return os.path.expanduser(os.path.join('~', basename+'.rec'))
 
-	@staticmethod
-	def load(fname):
-		checkpoints = []
-		with open(fname, 'r') as f:
-			lines = []
-			for line in f:
-				if line == 'CHECKPOINT\n':
-					checkpoints.append(''.join(lines))
-					lines = []
-				else:
-					lines.append(line)
-			if lines:
-				checkpoints.append(''.join(lines))
-		return checkpoints
+class Recording(object):
+
+	def __init__(self, view):
+		dirname, basename = os.path.split(view.file_name())
+		self.fname = os.path.expanduser(os.path.join('~', basename+'.rec'))
+		self.lines = None
+
+	def load(self):
+		with open(self.fname, 'r') as f:
+			self.lines = f.readlines()
+
+	def ops(self):
+		if not self.lines:
+			self.load()
+		state = []
+		oldState = ''
+		for line in self.lines:
+			if line.startswith('CHECKPOINT'):
+				newState = ''.join(state)
+				state = []
+				for op in genops(oldState, newState):
+					yield op
+				oldState = newState
+			elif line.startswith('COMMAND'):
+				cmd = line[len('COMMAND '):].strip()
+				for c in cmd:
+					yield ('command', {'cmd': c})
+				yield ('command', {'cmd': '\n'})
+				yield ('pause', None)
+			else:
+				state.append(line)
+
+
+def genops(oldState, newState):
+	diffs = difflib.ndiff(oldState, newState)
+	point = 0
+	for diff in diffs:
+		if diff[:2] == '  ':
+			point += 1
+		elif diff[:2] == '+ ':
+			yield ('insert', {'point': point, 'text': diff[2:]})
+			point += 1
+		elif diff[:2] == '- ':
+			yield ('erase', {'point': point})
+	yield ('pause', None)
 
 
 class PlaybackRecordingCommand(sublime_plugin.TextCommand):
@@ -110,22 +163,17 @@ class PlaybackRecordingCommand(sublime_plugin.TextCommand):
 	ops = None
 	count = 0
 
-	def run(self, edit, clear=False):
+	def run(self, edit, start=False, clear=False):
 		if clear:
 			PlaybackRecordingCommand.ops = None
-		elif not PlaybackRecordingCommand.ops:
+		elif start and not PlaybackRecordingCommand.ops:
 			self.view.erase(edit, sublime.Region(0, self.view.size()))
-			PlaybackRecordingCommand.ops = genops(Recording.load(Recording.name(self.view)))
+			PlaybackRecordingCommand.ops = Recording(self.view).ops()
 			PlaybackRecordingCommand.count = 0
-			self.view.run_command('playback_recording')
-		else:
+			sublime.set_timeout_async(lambda: self.view.run_command('playback_recording'), 3000)
+		elif PlaybackRecordingCommand.ops:
 			self.view.run_command('enter_insert_mode')
 			try:
-				PlaybackRecordingCommand.count += 1
-				if PlaybackRecordingCommand.count % 2 == 0:
-					sublime.status_message('/')
-				else:
-					sublime.status_message('\\')
 				optype, opargs = next(PlaybackRecordingCommand.ops)
 				if optype == 'insert':
 					point, text = opargs['point'], opargs['text']
@@ -143,31 +191,15 @@ class PlaybackRecordingCommand(sublime_plugin.TextCommand):
 					self.view.run_command('save')
 					return
 				elif optype == 'command':
-					settings = sublime.load_settings('HitRecord.sublime-settings')
-					ttyecho = settings.get('ttyecho')
-					if ttyecho:
-						tty = settings.get('tty')
-						cmd = opargs['cmd']
-						if subprocess.call([ttyecho, '-n', tty, cmd], shell=True):
-							sublime.error_message('command failed: ' + cmd)
-					else:
-						print('ignoring command: no ttyecho set')
-				sublime.set_timeout_async(lambda: self.view.run_command('playback_recording'), 50)
+					self.view.run_command('exec_in_shell', {'command': opargs['cmd']})
+				PlaybackRecordingCommand.count += 1
+				if PlaybackRecordingCommand.count % 2 == 0:
+					sublime.status_message('/')
+				else:
+					sublime.status_message('\\')
+				sublime.set_timeout_async(lambda: self.view.run_command('playback_recording'), 100)
 			except StopIteration:
 				sublime.status_message('\\o/')
 				PlaybackRecordingCommand.ops = None
-
-def genops(checkpoints):
-	for i, newState in enumerate(checkpoints[1:]):
-		oldState = checkpoints[i]
-		diffs = difflib.ndiff(oldState, newState)
-		point = 0
-		for diff in diffs:
-			if diff[:2] == '  ':
-				point += 1
-			elif diff[:2] == '+ ':
-				yield ('insert', {'point': point, 'text': diff[2:]})
-				point += 1
-			elif diff[:2] == '- ':
-				yield ('erase', {'point': point})
-		yield ('pause', None)
+		else:
+			sublime.status_message('playback finished')
